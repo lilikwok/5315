e9e38b2a2effb84aed0ba3e3cf43fce116a73540
lilikwok <guoqianhui@seattleu.edu>
2019-04-20 21:59:34 -0700

    files

--- empdata.js
+++ empdata.js
@@ -1,11 +1,12 @@
 {
   "data": [
 
+   
     {
       "studentID": "B4045000",
       "name": "Robert Adams",
       "department": "Business",
-      "age": 25,
+      "age": "25",
       "startDate": "2017/09/01",
       "degree": "Bachelor"
 
@@ -15,7 +16,7 @@
       "studentID": "B4045001",
       "name": "Lili Kwok",
       "department": "Computer Science",
-      "age": 25,
+      "age": "25",
       "startDate": "2017/09/01",
       "degree": "Bachelor"
 
@@ -25,7 +26,7 @@
       "studentID": 'B4045002',
       "name": "Shirley Pretty",
       "department": "Biology",
-      "age": 21,
+      "age": "21",
       "startDate": "2017/03/01",
       "degree": "Bachelor"
 
@@ -35,7 +36,7 @@
       "studentID": 'B4045003',
       "name": "Galen Green",
       "department": "Engineer",
-      "age": 18,
+      "age": "18",
       "startDate": "2015/09/01",
       "degree": "Bachelor"
 
@@ -45,7 +46,7 @@
       "studentID": 'B4045004',
       "name": "Alen Lee",
       "department": "Art",
-      "age": 23,
+      "age": "23",
       "startDate": "2015/09/01",
       "degree": "Bachelor"
 
@@ -55,7 +56,7 @@
       "studentID": 'B4045005',
       "name": "Yumiko Gao",
       "department": "Biology",
-      "age": 21,
+      "age": "21",
       "startDate": "2018/03/01",
       "degree": "Bachelor"
 
@@ -65,7 +66,7 @@
       "studentID": 'B4045006',
       "name": "Jamie Brown",
       "department": "Computer Science",
-      "age": 20,
+      "age": "20",
       "startDate": "2016/03/01",
       "degree": "Bachelor"
 
@@ -75,7 +76,7 @@
       "studentID": 'M3036001',
       "name": "Ben Kim",
       "department": "Art",
-      "age": 20,
+      "age": "20",
       "startDate": "2017/09/01",
       "degree": "Master"
 
@@ -85,7 +86,7 @@
       "studentID": 'M3036002',
       "name": "Laurence Cory",
       "department": "Art",
-      "age": 27,
+      "age": "27",
       "startDate": "2018/09/01",
       "degree": "Master"
 
@@ -95,7 +96,7 @@
       "studentID": 'M3036003',
       "name": "Kevin Gasper",
       "department": "Business",
-      "age": 18,
+      "age": "18",
       "startDate": "2017/09/01",
       "degree": "Master"
 
@@ -105,7 +106,7 @@
       "studentID": 'M3036004',
       "name": "Dale Green",
       "department": "Business",
-      "age": 30,
+      "age": "30",
       "startDate": "2018/09/01",
       "degree": "Master"
 
@@ -115,7 +116,7 @@
       "studentID": 'M3036005',
       "name": "Susan White",
       "department": "Engineer",
-      "age": 19,
+      "age": "19",
       "startDate": "2017/03/01",
       "degree": "Master"
 
@@ -125,7 +126,7 @@
       "studentID": 'M3036006',
       "name": "Clair Pan",
       "department": "Engineer",
-      "age": 25,
+      "age": "25",
       "startDate": "2015/03/01",
       "degree": "Master"
 
@@ -135,7 +136,7 @@
       "studentID": 'M3036007',
       "name": "Yen Lee",
       "department": "Art",
-      "age": 19,
+      "age": "19",
       "startDate": "2016/09/01",
       "degree": "Master"
 
@@ -145,7 +146,7 @@
       "studentID": 'M3036008',
       "name": "Mark Beech",
       "department": "Computer Science",
-      "age": 21,
+      "age": "21",
       "startDate": "2018/09/01",
       "degree": "Master"
 
@@ -155,7 +156,7 @@
       "studentID": 'M3036009',
       "name": "Joe Lee",
       "department": "Computer Science",
-      "age": 20,
+      "age": "20",
       "startDate": "2015/09/01",
       "degree": "Master"
     }
