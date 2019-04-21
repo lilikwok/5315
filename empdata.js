8f4f9804f9d557f52b0dbef402f07c90aa98f928
lilikwok <guoqianhui@seattleu.edu>
2019-04-20 22:37:52 -0700

    files

--- empdata.js
+++ empdata.js
@@ -15,54 +15,6 @@
       "age": "25",
       "startDate": "2017/09/01",
       "degree": "Bachelor"
-    },
-    {
-      "studentID": 'B4045002',
-      "name": "Shirley Pretty",
-      "department": "Biology",
-      "age": "21",
-      "startDate": "2017/03/01",
-      "degree": "Bachelor"
-    },
-    {
-      "studentID": 'B4045003',
-      "name": "Galen Green",
-      "department": "Engineer",
-      "age": "18",
-      "startDate": "2015/09/01",
-      "degree": "Bachelor"
-    },
-    {
-      "studentID": 'B4045004',
-      "name": "Alen Lee",
-      "department": "Art",
-      "age": "23",
-      "startDate": "2015/09/01",
-      "degree": "Bachelor"
-    },
-    {
-      "studentID": 'B4045005',
-      "name": "Yumiko Gao",
-      "department": "Biology",
-      "age": "21",
-      "startDate": "2018/03/01",
-      "degree": "Bachelor"
-    },
-    {
-      "studentID": 'B4045006',
-      "name": "Jamie Brown",
-      "department": "Computer Science",
-      "age": "20",
-      "startDate": "2016/03/01",
-      "degree": "Bachelor"
-    },
-    {
-      "studentID": 'M3036001',
-      "name": "Ben Kim",
-      "department": "Art",
-      "age": "20",
-      "startDate": "2017/09/01",
-      "degree": "Master"
     }
   ]
 }
